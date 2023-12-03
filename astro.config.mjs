f011f31462b6121be4ee3efbd965bab881a7d70c
Leo Wilkin <64415791+leowilkin@users.noreply.github.com>
2023-12-03 16:52:58 +0000

    v1.0

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,17 +6,35 @@ export default defineConfig({
 	integrations: [
 		starlight({
 			title: 'Concepto AI',
+			lastUpdated: true,
+			favicon: '/favicon.svg',
+			customCss: [
+				'./src/styles/custom.css',
+			  ],
+			logo: {
+				dark: './src/assets/logo-light.svg',
+				light: './src/assets/logo-dark.svg',
+				replacesTitle: true,
+			},
 			social: {
-				github: 'https://github.com/leowilkin/concepto',
+				github: 'https://github.com/leowilkin/concepto-docs',
 			},
 			sidebar: [
 				{
-					label: 'Concepto AI',
-					autogenerate: { directory: 'concepto' },
+					label: 'About',
+					autogenerate: { directory: 'about' },
+					collapsed: true,
 				},
 				{
-					label: 'Beta Access',
-					autogenerate: { directory: 'beta-access '},
+					label: 'Self Hosted',
+					autogenerate: { directory: 'self-hosted' },
+					collapsed: true,
+				},
+				{
+					label: 'Cloud Hosted',
+					autogenerate: { directory: 'cloud-hosted' },
+					badge: 'BETA',
+					collapsed: true,
 				},
 			],
 		}),
